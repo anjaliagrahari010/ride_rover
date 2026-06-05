b08eb63094473ed0ba3d6f82f06aa053f263b86f
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-06 04:16:53 +0530

    Fix Render deployment

--- ride_rover/settings.py
+++ ride_rover/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0oy298zhlqkjiho&6$&b7j*v8$d0bbu*-^jaar272fver9bjt^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com",]
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
