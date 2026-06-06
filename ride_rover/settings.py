f99227057d26115c1212796b27ba760d46b6f68d
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-07 01:11:26 +0530

    database fixed

--- ride_rover/settings.py
+++ ride_rover/settings.py
@@ -109,11 +109,19 @@ WSGI_APPLICATION = 'ride_rover.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
