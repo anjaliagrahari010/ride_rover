02cb30b22fef67b58bd7adab2addf77bf15cf786
Anjali Agrahari <anjaliagrahari38@gmail.com>
2026-06-07 00:23:31 +0530

    fix: database

--- ride_rover/settings.py
+++ ride_rover/settings.py
@@ -41,14 +41,6 @@ cloudinary.config(
 
 
 
-
-
-
-
-
-
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -115,11 +107,12 @@ WSGI_APPLICATION = 'ride_rover.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
